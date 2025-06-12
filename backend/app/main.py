aab9b526f490360300e111e775257622830e880f
kushal <kushalb2005@gmail.com>
2025-06-12 15:30:01 +0530

    fixing frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from app.routers import user
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
 app= FastAPI()
 
 app.include_router(user.router)
--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -1,15 +1,17 @@
-from fastapi import APIRouter, status, HTTPException, Depends,Body
+from fastapi import APIRouter, status, HTTPException, Depends,Body, Request
+from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from app.schemas.user import UserResponseMessage, UserCreate
 from app.database import get_session
 from app.crud.crud import add_user, remove_user
 from pydantic import ValidationError
 from typing import Annotated
-
 router=APIRouter(tags=["user"])
 
+
+
 @router.post("/subscribe", response_model=UserResponseMessage, status_code=status.HTTP_201_CREATED)
-def create_user(user:Annotated[UserCreate, Body()], session:Session=Depends(get_session)):
+def create_user(request:Request, user:Annotated[UserCreate, Body()], session:Session=Depends(get_session)):
     try:
         add_user(user=user, session=session)
         return UserResponseMessage(msg=f"{user.name} is added to newsletter")
